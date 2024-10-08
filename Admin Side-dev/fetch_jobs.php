acade00a14c255055f61666f3dd6627c440501e8
Juan Escalante <osiris0942@gmail.com>
2024-10-08 20:57:48 +0800

    AdminSide Jobs Search Bar Complete

--- Admin Side-dev/fetch_jobs.php
+++ Admin Side-dev/fetch_jobs.php
@@ -4,6 +4,18 @@ include_once("connection.php");
 $conn = connection();
 
 $status = isset($_GET['status']) ? $_GET['status'] : '';
+$sortBy = isset($_GET['sort_by']) ? $_GET['sort_by'] : 'date'; // Default sort by date
+$order = isset($_GET['order']) ? $_GET['order'] : 'asc'; // Default order ascending
+
+// Map sort options to database fields
+$sortOptions = [
+    'date' => 'j.date_posted',
+    'company' => 'j.company_name',
+    'title' => 'j.job_title'
+];
+
+$sortField = array_key_exists($sortBy, $sortOptions) ? $sortOptions[$sortBy] : 'j.date_posted';
+$order = ($order === 'desc') ? 'DESC' : 'ASC';
 
 // Updated SQL query to join job_table with partner_table to fetch the logo
 $sql = "SELECT j.company_name, p.logo AS company_logo, j.id, j.job_title, j.job_location, j.date_posted, j.job_candidates, j.job_status 
@@ -16,6 +28,9 @@ if ($status) {
     $sql .= " WHERE j.job_status = ?";
     $params[] = $status;
 }
+
+// Add sorting to the query
+$sql .= " ORDER BY $sortField $order";
 
 $stmt = $conn->prepare($sql);
 

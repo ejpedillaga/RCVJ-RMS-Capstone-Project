6f6f78fbbd42268e5bbf4f1c9025cc4cfda1a4ef
Juan Escalante <osiris0942@gmail.com>
2024-10-18 23:33:25 +0800

    Added time on schedules

--- Admin Side-dev/add_schedule.php
+++ Admin Side-dev/add_schedule.php
@@ -9,15 +9,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $company_name = $_POST['company_name'];
     $candidate_name = $_POST['candidate_name'];
     $scheduled_date = $_POST['scheduled_date'];
+    $start_time = $_POST['start_time'];  // New start time field
+    $end_time = $_POST['end_time'];      // New end time field
 
     // Begin transaction to ensure both operations (insert and update) are successful
     mysqli_begin_transaction($conn);
 
     try {
         // Insert the new schedule into the schedule_table
-        $query = "INSERT INTO schedule_table (job_title, company_name, candidate_name, scheduled_date) VALUES (?, ?, ?, ?)";
+        $query = "INSERT INTO schedule_table (job_title, company_name, candidate_name, scheduled_date, start_time, end_time) VALUES (?, ?, ?, ?, ?, ?)";
         $stmt = mysqli_prepare($conn, $query);
-        mysqli_stmt_bind_param($stmt, 'ssss', $job_title, $company_name, $candidate_name, $scheduled_date);
+        mysqli_stmt_bind_param($stmt, 'ssssss', $job_title, $company_name, $candidate_name, $scheduled_date, $start_time, $end_time);
 
         if (!mysqli_stmt_execute($stmt)) {
             throw new Exception(mysqli_error($conn));
